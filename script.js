88342698edbac6f3f90f18ed8969559db0c4e8e1
adistyningrum <147791586+adistyningrum@users.noreply.github.com>
2023-11-29 21:23:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function () {
+    $(".navbar a[href='#about-me']").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+  $(document).ready(function () {
+    $(".navbar a[href='#work']").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+  $(document).ready(function () {
+    $(".navbar a[href='#contact']").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
